b78be7f4fd6640c4aed531658fa9d32a2dc056e2
HamimBhai742 <omagootruelove147@gmail.com>
2024-06-03 14:13:57 +0600

    api update

--- index.js
+++ index.js
@@ -109,6 +109,19 @@ async function run() {
             const result = await userCollection.updateOne(filter, updateDoc);
             res.send(result)
         })
+
+        app.patch('/users/:id', async (req, res) => {
+            const id = req.params.id
+            console.log(id);
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'User'
+                },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result)
+        })
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
